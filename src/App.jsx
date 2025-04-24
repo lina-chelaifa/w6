b5f515aacd6bb9ca1c9a8a602af8fcf64befa7fb
lina <chelaifalina@gmail.com>
2025-04-24 20:34:20 +0100

    Updated Week 6- Lina

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,64 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+const App=() => { 
+const stories = [ 
+{ 
+title: 'React', 
+url: 'https://reactjs.org/', 
+author:'Jordan Walke' , 
+num_comments:3, 
+points:4, 
+objectID:0, 
+} ,
+{ 
+title: 'Redux', 
+url: 'https://redux.js.org/', 
+author: 'Dan Abramov, Andrew Clark', 
+num_comments: 2, 
+points :5, 
+objectID: 1, 
+},
+];
+return( 
+<div> 
+  <h1>My Hacker Stories</h1> 
+  <Search /> 
+  <hr /> 
+  <List list={stories} /> 
+</div> 
+); 
+};
+const List = (props) => ( 
+  <ul> 
+  {props.list.map((item) => ( 
+  <Item key={item.objectID} item={item} /> 
+  ))} 
+  </ul> 
+  ); 
+  const Item = (props) => ( 
+  <li> 
+  <span> 
+  <a href={props.item.url}>{props.item.title} </a> 
+  </span> 
+  <span>{props.item.author} </span> 
+  <span>{props.item.num_comments} </span> 
+  <span>{props.item.points} </span> 
+  </li> 
+  );
+  const Search = () => { 
+    const [searchTerm, setSearchTerm] = React.useState(''); 
+    const handleChange =(event) => { 
+    setSearchTerm (event.target.value); 
+    }; 
+    return ( 
+    <div> 
+    <label htmlFor="search">Search: </label> 
+    <input id="search" type="text" onChange={handleChange} /> 
+    <p> 
+    Searching for <strong> (searchTerm)</strong>. 
+    </p> 
+    </div> 
+    ); 
+    };
+    // week 6 commit
